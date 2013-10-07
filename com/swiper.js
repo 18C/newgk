c451bca6e126dc7f61d00fe9bd17a606d73df24a
ccfang <ccfang@icsc.com.tw>
2013-10-07 10:52:43 +0800

    refactor swiper.js and add swiper_nav.html example

--- com/swiper.js
+++ com/swiper.js
@@ -1,40 +1,47 @@
 requirejs.config({
 	context: 'gk',
 	paths: {
-		'idangerous.swiper': 'lib/swiper/idangerous.swiper-2.1.min',
+		'idangerous.swiper': 'lib/swiper/idangerous.swiper-2.1.min'
 	}
 });
 
 // define module (component)
-define(
-	'swiper', ['idangerous.swiper'],
-	function() {
-		return {
-			template: "<div id='{{id}}' class='swiper-container'><div class='swiper-wrapper'><content></content></div></div>",
-			script: function() {
-				var $oriEle = this.$originEle;
-				var $ele = this.$ele;
+define('swiper', ['idangerous.swiper'], function() {
+	return {
+		template: "<div id='{{id}}' class='swiper-container'><div class='swiper-wrapper'><content></content></div></div>",
+		script: function() {
+			var $oriEle = this.$originEle;
+			var $ele = this.$ele;
 
-				var defaults = {
-					'grabCursor': true,
-					'onSlideChangeStart': function() {
-						$ele.triggerHandler("slideChangeStart", arguments)
-					}
-				};
-
-				var pagination = $('.pagination');
-				var pagination_defaults;
-				if (pagination.length > 0) {
-					pagination_defaults = {
-						'pagination': pagination.selector,
-						'paginationClickable': true
-					};
+			var defaults = {
+				'onSlideChangeStart': function() {
+					$ele.triggerHandler("onSlideChangeStart", arguments);
+				},
+				'onSlideClick': function() {
+					$ele.triggerHandler("onSlideClick", arguments);
 				}
+			};
 
-				this.init = function() {
-					var settings = $.extend({}, defaults, pagination_defaults);
-					this.swiper =  $ele.swiper(settings);					
+			var config = $.extend(defaults, {
+				freeMode: $oriEle.attr("freeMode") === 'true',
+				freeModeFluid: $oriEle.attr("freeModeFluid") === 'true',
+				grabCursor: $oriEle.attr("grabCursor") === 'true',
+				slidesPerView: $oriEle.attr('slidesPerView')
+			});
+
+			var pagination = $('.pagination');
+			var pagination_defaults;
+			if (pagination.length > 0) {
+				pagination_defaults = {
+					'pagination': pagination.selector,
+					'paginationClickable': true
 				};
 			}
-		};
-	});
+
+			this.init = function() {
+				var settings = $.extend({}, defaults, config, pagination_defaults);
+				this.swiper = $ele.swiper(settings);
+			};
+		}
+	};
+});
